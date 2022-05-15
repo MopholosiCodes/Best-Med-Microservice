04689a612bf86dfa1a30ff39ed260b5f0ba84cf1
Mopholosi <67508554+Mopholosi@users.noreply.github.com>
2022-05-15 13:34:19 +0200

    Version3.0

---
+++ WebService/PremiumCalculator/Index.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace PremiumCalculator
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class Index : System.Web.Services.WebService
+    {
+        private float penaltyPremium;
+
+        [WebMethod]
+        public string CalculatePremium(string name, int age, float income, int adultDependents, int childDependents)
+        {
+            PremiumCalculator Calculator = new PremiumCalculator();
+            Index program = new Index();
+
+            var premuim = Calculator.calcMonthlyPremium(income, childDependents, adultDependents);
+            var monthlyPremium = premuim;
+            var premiumPenalties = Calculator.calcPremiumPenalties(program.penaltyPremium, age, monthlyPremium);
+
+            return $"{name} (age {age}) has {adultDependents} adult dependents and {childDependents} child dependents. " +
+                        $"He/She will pay R{premiumPenalties} p/Month";
+        }
+    }
+}
